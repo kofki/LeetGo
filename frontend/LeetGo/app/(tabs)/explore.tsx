ca4c15a626fc13fbd2771e16a2d8f57610fde3c4
kofki <69553679+kofki@users.noreply.github.com>
2025-06-14 00:12:28 -0400

    Added the Multiple Choice UI with Dark Theme with example palindrome

--- frontend/LeetGo/app/(tabs)/explore.tsx
+++ frontend/LeetGo/app/(tabs)/explore.tsx
@@ -1,110 +1,82 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState } from 'react';
 
 export default function TabTwoScreen() {
+  const [selectedWord, setSelectedWord] = useState<string | null>(null);
+  
+  const words= ['car', 'racecar', 'LeetGo', 'cat'];
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <ThemedView style={styles.container}>
+      <View style={[styles.progressContainer]}>
+              <View style={styles.progressBar} />
+              <Text style={styles.hearts}>❤️ 5</Text>
+            </View>
+            <Text style={styles.instruction}>Which of these words is a palindrome?</Text>
+            <FlatList data={words}
+                      renderItem={({ item }) => {
+                        const isSelected = selectedWord === item;
+                        return(
+                        <TouchableOpacity style={[styles.wordButton, isSelected && styles.wordButtonSelected]} onPress={() =>setSelectedWord(item)}>
+                          <Text style={styles.wordText}>{item}</Text>
+                        </TouchableOpacity>
+                        );
+                    }}
+                      keyExtractor={(item) => item}/>
+            <TouchableOpacity style={styles.checkButton} onPress={()=> console.log(`Selected word: ${selectedWord}`)}>
+                    <Text style={styles.checkButtonText}>CHECK</Text>
+            </TouchableOpacity>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
+  container: { padding: 20, paddingTop: 70, flex: 1, color: 'white'},
+  progressContainer: {
     flexDirection: 'row',
-    gap: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    gap: 10,
+  },
+  progressBar: {
+    marginLeft: 45,
+    marginRight: 10,
+    marginBottom: 20,
+    width: 275,
+    height: 20,
+    backgroundColor: '#1ae617',
+    borderRadius: 20,
+  },
+  hearts: { fontSize: 16, color: 'white' },
+  instruction: {  fontSize: 18, fontWeight: 'bold', marginVertical: 10, color: 'white' },
+
+  wordButton : {
+    backgroundColor: 'grey',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 6,
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wordButtonSelected: {
+    backgroundColor: '#4CAF50', // green highlight
+  },
+  wordText: {
+    color: 'white',
+  },
+
+  checkButton: {
+    marginTop: 20,
+    backgroundColor: 'lightgreen',
+    paddingVertical: 12,
+    borderRadius: 6,
+    alignItems: 'center',
+  },
+  checkButtonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
